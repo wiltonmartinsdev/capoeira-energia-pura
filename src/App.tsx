2298f1b7a8ec21946e10fb7558e58db344fdc5ec
Wilton Martins <wiltonliramartins@icloud.com>
2024-07-30 14:06:46 -0300

    Style: Creating the application header

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,13 @@
 function App() {
 
   return (
-    <h1 className="bg-red-500">Hello World!</h1>
+    <>
+        <header>
+            <nav></nav>
+        </header>
+
+        <h1>Capoeira Energia Pura</h1>
+    </>
   )
 }
 
