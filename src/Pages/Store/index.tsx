56830d09432045f216404d6ef93b735840e09ade
Wilton Martins <wiltonliramartins@icloud.com>
2024-11-01 20:47:54 -0300

    feat: Add construction message and icons to Store page

--- src/Pages/Store/index.tsx
+++ src/Pages/Store/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "@/components/Header";
 import { LanguageSelection } from "@/components/LanguageSelection";
+import ConstructionIcon from "@/assets/contructionIcon.svg";
+import RocketIcon from "@/assets/rocketIcon.svg";
 
 export function Store() {
 	return (
@@ -10,7 +12,24 @@ export function Store() {
 				<Header />
 			</div>
 
-			<main className="min-w-80 xl:max-w-[1920px] mx-auto"></main>
+			<main className="min-w-80 xl:max-w-[1920px] mx-auto h-screen flex justify-center">
+				<section className="font-roboto-flex font-black text-green-900 flex items-center gap-6">
+					<img
+						src={ConstructionIcon}
+						alt=""
+					/>
+
+					<h1 className="text-2xl">
+						Esta página está em construção. Volte em breve para ver
+						as novidades!
+					</h1>
+
+					<img
+						src={RocketIcon}
+						alt=""
+					/>
+				</section>
+			</main>
 		</>
 	);
 }
